5b91b078f7b607a56c536bc5d54ac27fe2eda87b
gaur-mohit <mohit3g2a1u0r@gmail.com>
2023-05-17 14:12:04 +0000

    user deshboard after login and edit profile

---
+++ frontend/src/pages/Tracking.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../layout/Navbar'
+
+const Tracking = () => {
+  return (
+    <div>
+      <header>
+        <Navbar />
+      </header>
+      Tracking
+    </div>
+  )
+}
+
+export default Tracking
